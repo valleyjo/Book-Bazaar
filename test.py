a35354a1bf9047c3cfbc665f2748cead2fdc142f
Alex Vallejo <vallejo.alex@gmail.com>
2014-09-24 15:33:06 -0400

    Test push to github to deploy

--- test.py
+++ test.py
@@ -2,41 +2,6 @@ import webapp2
 import os
 from google.appengine.ext.webapp import template
 
-class GradeQuiz(webapp2.RequestHandler) :
-  def post(self) :
-    answer1 = self.request.get('q1')
-    answer2 = self.request.get('q2')
-    answer3 = self.request.get('q3')
-    myHtml = '''
-<html>
-  <head>
-    <link rel="stylesheet" href="css/style.css">
-  <script src="scripts/script.js">
-  </script>
-  </head>
-  <body>
-    <h1>QUIZ TIME!</h1>
-'''
-
-    if (answer1 == 'q1a2') :
-      myHtml += 'Question 1 is correct.<br>'
-    else :
-      myHtml += 'Question 1 is not correct.<br>'
-
-    if (answer2 == 'q2a1') :
-      myHtml += 'Question 2 is correct.<br>'
-    else :
-      myHtml += 'Question 2 is not correct.<br>'
-
-    if (answer3 == 'q3a3') :
-      myHtml += 'Question 3 is correct.<br>'
-    else :
-      myHtml += 'Question 3 is not correct.<br>'
-
-    myHtml += '</body></html>'
-    self.response.out.write(myHtml)
-
-
 class MainPage(webapp2.RequestHandler) :
   def get(self) :
     myHtml = '''
@@ -48,6 +13,7 @@ class MainPage(webapp2.RequestHandler) :
   </head>
   <body>
     <h1>CS1520 Group 8</h1>
+    <p>This is a test of the Github push to deploy feature</p>
   </body>
 </html>
 '''
@@ -55,7 +21,6 @@ class MainPage(webapp2.RequestHandler) :
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/submitquiz', GradeQuiz)
 ], debug=True)
 
 app.run()
